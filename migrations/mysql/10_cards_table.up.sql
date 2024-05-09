0730493d6728f0c57f9154a2609e8e0927076161
MelnikovNA <stif1550@gmail.com>
2024-05-09 12:56:54 +0300

    sql fix

--- migrations/mysql/10_cards_table.up.sql
+++ migrations/mysql/10_cards_table.up.sql
@@ -4,4 +4,6 @@ CREATE TABLE IF NOT EXISTS `cards` (
   `eng` varchar(255) NOT NULL,
   `Transcription` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
+  INDEX(`rus`),
+  INDEX(`eng`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
