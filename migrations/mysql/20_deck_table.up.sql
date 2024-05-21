fff6d49ad6dbf91d871794b1e2693eff7a022ea8
MelnikovNA <stif1550@gmail.com>
2024-05-21 15:20:19 +0300

    fix decks

--- migrations/mysql/20_deck_table.up.sql
+++ migrations/mysql/20_deck_table.up.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS `decks` (
   `deck_id` varchar(255) NOT NULL,
   `card_id` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `deck_id_2` (`deck_id`, `card_id`),
   INDEX(`deck_id`),
   INDEX(`card_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
